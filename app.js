f24d2bbdd398b0b179c8f7bc6de9d9e7cdb65aab
kalelov555 <73077918+kalelov555@users.noreply.github.com>
2021-06-26 21:10:57 +0600

    add headers

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 
 app.get('/', function(req, res) {
     const url = "https://www.metaweather.com/api/location/44418";
